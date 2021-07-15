ebc542298c04ce5fafd0c947304763420bf4c578
Romit <romitkumar_singh@yahoo.com>
2021-07-15 14:43:48 +0530

    commenting out plugin for now

--- src/test/java/Runners/Run.java
+++ src/test/java/Runners/Run.java
@@ -6,8 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/firstTest.feature",
         glue = {"StepDefinitions"},
-        tags = "@regression",
-        plugin = {"pretty", "json:target/cucumber-report.json" }
+        tags = "@regression"
+//        plugin = {"pretty", "json:target/cucumber-report.json" }
 
 )
 public class Run extends AbstractTestNGCucumberTests {
